94f39dadccacbada4aff4a6761205daeb716b43f
asred <asred@LAPTOP-9HART8E0>
2021-08-12 14:03:49 +0530

    ds

---
+++ OrangeHRM010/src/main/java/OrangeHRM010/OrangeHRM010/App.java
@@ -0,0 +1,13 @@
+package OrangeHRM010.OrangeHRM010;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
